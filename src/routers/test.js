8ef8dd27f602f2b59bf68bbe200fae81d1f85d92
ManzarBilal <manzarbilal@gmail.com>
2023-08-12 17:16:40 +0530

    gfd

--- src/routers/test.js
+++ src/routers/test.js
@@ -1,1 +1,20 @@
-// dfghjkl
+const express=require("express");
+const router= express.Router();
+
+
+
+router.post("/login",async(req,res)=>{
+    try{
+      let body=req.body;
+      let user= await Registration.findOne({email:body.email,password:body.password});
+      if(user){
+        res.send({status:true,msg:"Logged in",user:user});
+      }else{
+      res.status(404).send({status:false,msg:"Incorrect Username and Password!"});
+      }
+    }catch(err){
+      res.status(400).send(err);
+    }
+});
+
+module.exports=router;
